b09fa51c7ba8f68969c30829c507800b41699281
whojaskaran <jaskaransingh.3015@gmail.com>
2025-12-15 23:14:46 +0530

    hello

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,13 +1,15 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+const path = require("path");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
+  mode: "production",
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/',
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/",
+    clean: true,
   },
   module: {
     rules: [
@@ -15,30 +17,30 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
+            presets: ["@babel/preset-env", "@babel/preset-react"],
           },
         },
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html',
-      filename: 'index.html',
+      template: "./public/index.html",
+      filename: "index.html",
+    }),
+    new webpack.DefinePlugin({
+      "process.env.REACT_APP_SERVER_URL": JSON.stringify(
+        process.env.REACT_APP_SERVER_URL
+      ),
     }),
   ],
-  devServer: {
-    historyApiFallback: true,
-    port: 3000,
-    open: true,
-  },
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: [".js", ".jsx"],
   },
 };
